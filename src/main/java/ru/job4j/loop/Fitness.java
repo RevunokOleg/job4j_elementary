fc99a3dc527cc5c5fb524d96d58fa7c52b83b1c0
Red Devil <revunokoleg@gmail.com>
2024-01-29 22:45:40 +0300

    5.4. Протеиновая диета (цикл while) [#156313]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Fitness {
+
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month += 1;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(90, 100));
+        System.out.println(calc(60, 100));
+        System.out.println(calc(190, 1000));
+        System.out.println(calc(9, 98));
+    }
+}
